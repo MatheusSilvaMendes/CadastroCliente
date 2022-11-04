d14b39cede4a21daff1f66d21222dccfc9785179
MatheusSilvaMendes <matheussm.090@gmail.com>
2022-11-04 17:18:29 -0300

    New connection string, criação do banco.

---
+++ TesteUPD8/Data/Migrations/20221104043000_removendo_tabela_sexo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TesteUPD8.Data.Migrations
+{
+    public partial class removendo_tabela_sexo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "clienteId",
+                table: "Clientes",
+                newName: "ClienteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ClienteId",
+                table: "Clientes",
+                newName: "clienteId");
+        }
+    }
+}
